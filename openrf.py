91b1cb999290d9c0915d0f5c55819189629ea425
Shiv Pratap Singh <106021529+Kirosci@users.noreply.github.com>
2023-06-27 17:58:34 +0530

    Update openrf.py, added multi threading

--- openrf.py
+++ openrf.py
@@ -5,7 +5,10 @@ import urllib.parse
 import argparse
 import requests
 from termcolor import colored
+from time import sleep
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
+REQUESTS_PER_SECOND = 10
 
 def replace_url_in_query_params(url, replace_url, replace_all):
     # decode the input URL if it is url encoded
@@ -22,7 +25,7 @@ def replace_url_in_query_params(url, replace_url, replace_all):
     else:
         # Replace only the URL in query parameters
         modified_url = re.sub(r'((?:^|&)[^&=]*?=)https?://[^&]*', r'\1' + replace_url, decoded_url)
-    
+
     return modified_url
 
 def make_request(url):
@@ -43,24 +46,35 @@ def make_request(url):
     except requests.exceptions.RequestException as e:
         return str(e)
 
+def process_url(url, args):
+    updated_url = replace_url_in_query_params(url.strip(), args.url, args.replace_all)
+    print(updated_url)
+    if args.request:
+        response = make_request(updated_url)
+        print(f"Status Code: {response}")
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Example: python3 openrf.py -f urls.txt -u https://serverurl/ -A -o output.txt')
     parser.add_argument('-f', '--file', type=str, required=True, help='Specify file containing URLs')
-    parser.add_argument('-u', '--url', type=str, required=True, help='URL for replacing the vlaues, For operedirect or ssrf\nBurp Collborator, postbin,etc')
+    parser.add_argument('-u', '--url', type=str, required=True, help='URL for replacing the values, For operedirect or ssrf\nBurp Collaborator, postbin, etc')
     parser.add_argument('-o', '--output', type=str, required=True, help='Output file to save results')
-    parser.add_argument('-A', '--replace-all', action='store_true', help='Replace values of all parameters in the url with given url\n Without -A it will only replace the parameters having any url as value')
-    parser.add_argument('-R', '--request', action='store_true', help='Make request to each modified URL and print the status code')
+    parser.add_argument('-A', '--replace-all', action='store_true', help='Replace values of all parameters in the URL with the given URL\n Without -A it will only replace the parameters having any URL as value')
+    parser.add_argument('-R', '--request', action='store_true', help='Make a request to each modified URL and print the status code')
     args = parser.parse_args()
 
     with open(args.file, 'r') as f:
         urls = f.readlines()
 
     with open(args.output, 'w') as f:
-        for url in urls:
-            updated_url = replace_url_in_query_params(url.strip(), args.url, args.replace_all)
-            f.write(updated_url + '\n')
-            print(updated_url)
-            if args.request:
-                response = make_request(updated_url)
-                print(f"Status Code: {response}")
+        with ThreadPoolExecutor(max_workers=REQUESTS_PER_SECOND) as executor:
+            futures = []
+            for url in urls:
+                future = executor.submit(process_url, url, args)
+                futures.append(future)
+                if args.request:
+                    sleep(1 / REQUESTS_PER_SECOND)
+
+            for future in as_completed(futures):
+                pass
+print("All requests completed.")
